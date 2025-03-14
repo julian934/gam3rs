18685a5dc9e4cbb6a72638e427fc8228f2cb3b52
julian934 <102177301+julian934@users.noreply.github.com>
2025-03-13 22:00:18 -0500

    upload 2.0

--- src/app/api/cloudinary/getProfilePic/route.ts
+++ src/app/api/cloudinary/getProfilePic/route.ts
@@ -1,9 +1,2 @@
 import { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
-
-export async function getProfilePic(request:NextRequest){
-
-    return NextResponse.json({message:"Data not found"}, {status:500})
-}
-
-export {getProfilePic as GET}
