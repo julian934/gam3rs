7a5400d5e62f1fcd6d36b3c9cef06d825c268e68
julian934 <102177301+julian934@users.noreply.github.com>
2025-03-14 12:32:09 -0500

    upload 2.9

--- src/app/api/videos/getupcomingvideos/route.tsx
+++ src/app/api/videos/getupcomingvideos/route.tsx
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
 import { ObjectId } from "mongodb";
 
-export async function getUpcomingVids(request:NextRequest){
+export async function GET(request:NextRequest){
     const client=await new MongoClient(`${process.env.NEXT_PUBLIC_MONGO_DB}`);
     const id=await new ObjectId(`${process.env.NEXT_PUBLIC_MONGO_OBJECT_ID}`);
     try {
@@ -22,4 +22,4 @@ export async function getUpcomingVids(request:NextRequest){
 
 }
 
-export {getUpcomingVids as GET}
+//export {getUpcomingVids as GET}
--- src/app/api/videos/route.tsx
+++ src/app/api/videos/route.tsx
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-export async function getVideos(request:NextRequest){
+export async function GET(request:NextRequest){
 
     return NextResponse.json({Data:'Data'})
 }
