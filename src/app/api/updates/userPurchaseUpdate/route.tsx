7146578518bbe451ecdd29a004ff332062457b56
julian934 <102177301+julian934@users.noreply.github.com>
2025-03-14 14:45:13 -0500

    upload 3.7

--- src/app/api/updates/userPurchaseUpdate/route.tsx
+++ src/app/api/updates/userPurchaseUpdate/route.tsx
@@ -1,4 +1,6 @@
 
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 export async function GET(){
-    
+    return NextResponse.json({data:'Info not found'})
 }
--- src/app/api/updates/userWishListUpdate/route.tsx
+++ src/app/api/updates/userWishListUpdate/route.tsx
@@ -1,3 +1,5 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 export async function GET(){
-    
+    return NextResponse.json({data:'Info not found'})
 }
