e6220620404ce7e4106dc6a474f71fe630d65066
julian934 <102177301+julian934@users.noreply.github.com>
2025-05-20 17:36:23 -0500

    new comm

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -37,11 +37,11 @@ const About = (props: Props) => {
      <div className='flex md:space-x-2 max-sm:flex-col grid z-60 md:-mb-12 md:p-4 grid-cols-4 grid-rows-4 row-start-2 md:self-center md:justify-self-center row-span-2 col-start-1 col-span-6  rounded-lg  ' >
      
       <div className=' flex  max-sm:col-start-1 max-sm:col-span-4 row-start-1 row-span-3 col-start-1 col-span-2
-        max-sm:row-start-1 max-sm:row-span-3 max-sm:col-start-1 max-sm:col-span-6 max-sm:h-full  self-center max-sm:z-50 max-sm:px-4' >
+        max-sm:row-start-1 max-sm:row-span-3 max-sm:col-start-1 max-sm:col-span-6 max-sm:h-full  self-center max-sm:z-50 max-sm:px-4 md:pt-20' >
           <About_Modal/>
       </div>
       <div className='flex flex-col row-start-1 row-span-4 justify-center col-start-3  self-center col-span-2 
-       max-sm:col-start-1 max-sm:col-span-6 max-sm:row-start-3 z-50 max-sm:justify-end max-sm:z-50 max-sm:self-start max-sm:mt-14 ' >
+       max-sm:col-start-1 max-sm:col-span-6 max-sm:row-start-3 z-50 max-sm:justify-end max-sm:z-50 max-sm:self-start mt-14 ' >
         <div className='flex self-center' >
             <h1 className=' flex  justify-center self-center p-2 text-6xl ' >What We Do</h1> {/* Add Design */}
         </div>
